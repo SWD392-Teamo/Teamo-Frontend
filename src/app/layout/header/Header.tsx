25b93282a168a8e8f15f5fae6816f51403de1fa7
r1nzl3rrr <khanhle9344@gmail.com>
2025-02-04 15:30:25 +0700

    Khanh Le - added header and custom tailwind classes #1

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -2,10 +2,10 @@ import LoginButton from './LoginButton';
 import Logo from './Logo';
 import NavLinks from './NavLinks';
 
-export default async function Navbar() {
+export default async function Header() {
     return (
         <header className='sticky top-0 z-50 bg-tertiary shadow-md items-center w-full'>
-            <div className='flex justify-between shadow-sm py-3 px-10'>
+            <div className='flex justify-between border-b-2 py-3 px-10'>
                 <Logo />
                 <LoginButton />
             </div>
