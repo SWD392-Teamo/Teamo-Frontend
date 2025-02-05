0b1c2fd473292e7a9e221005c6ad792ae6554fac
r1nzl3rrr <khanhle9344@gmail.com>
2025-02-05 16:40:36 +0700

    added fetch wrapper to and auth action for authentication actions #4

---
+++ src/app/actions/authActions.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import { fetchWrapper } from "@/lib/fetchWrapper";
+import { cookies } from 'next/headers';
+
+export async function login(email: string, password: string) {
+    const response = await fetchWrapper.post(`account/login`, {email: email, password: password});
+
+    if (response.cookies) {
+        const cookieStore = await cookies();
+
+        const cookieString = response.cookies;
+
+        const cookieParts = cookieString.split(';')[0]; // Only get the first part before ";"
+        const [cookieName, cookieValue] = cookieParts.split('=');
+        
+        cookieStore.set(cookieName.trim(), cookieValue.trim(), {
+            path: '/',       // Ensure it's accessible across the app
+            httpOnly: cookieString.toLowerCase().includes('httponly'),  // Check if HttpOnly is set
+            secure: cookieString.toLowerCase().includes('secure'),      // Check if Secure is set
+            sameSite: cookieString.toLowerCase().includes('samesite=none') ? 'none' : 'lax', // Check for SameSite policy or default to lax
+        });
+    }
+
+    return response.data;
+}
+
+export async function getCurrentUser() {
+    return await fetchWrapper.get(`account/user-info`);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,51 @@
+'use client'
+
+import { useEffect } from "react";
+import { getCurrentUser, login } from "./actions/authActions";
+
 export default function Home() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    console.log(email, password)
+
+    // var res = await login(email, password);
+
+    var res = await fetch('https://localhost:5001/api/account/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      credentials: 'include'
+    })
+
+    if (res.ok) console.log("ok")
+  };
+
+  const handleUser = async () => {
+    // Pass cookies to the server action
+    const res = await getCurrentUser();
+  
+    if (res) console.log(res);
+  };
+  
   return (
-    <h1 className="text-3xl">Teamo</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button type="submit">Sign In</button>
+      </form>
+
+      
+      <button onClick={handleUser}>Get User</button>
+      
+    </div>
+    
   );
 }
