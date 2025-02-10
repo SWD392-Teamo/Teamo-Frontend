14b2d3470bf6efed4631231966d896f880608bce
r1nzl3rrr <khanhle9344@gmail.com>
2025-02-11 00:02:51 +0700

    Khanh Le - added zustand for state management and paginations

--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+'use client'
+
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { useSubjectStore } from '@/hooks/useSubjectStore';
+import React, { useEffect, useState } from 'react'
+import { useShallow } from 'zustand/shallow';
+import { Subject } from '@/types';
+import queryString from 'query-string';
+import { getData } from '../actions/subjectAction';
+import Paginator from '../components/Paginator';
 
 export default function Listings() {
+  // Loading state
+  const [loading, setLoading] = useState(true);
+
+  // Put all params into a single object
+  const params = useParamsStore(useShallow(state => ({
+    pageIndex: state.pageIndex,
+    pageSize: state.pageSize,
+    search: state.search
+  })));
+
+  // Put all data into a paged object
+  const data = useSubjectStore(useShallow(state => ({
+    subjects: state.subjects,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount
+  })))
+
+  const setData = useSubjectStore(state => state.setData);
+  const setParams = useParamsStore(state => state.setParams)
+  
+  // Create a query string for fetching data with filters
+  const url = queryString.stringifyUrl({url: '', query: params})
+
+  function setPageIndex(pageIndex: number) {
+    setParams({pageIndex})
+  }
+
+  // Rerender component depends on pageNumber
+  useEffect(() => {
+    getData(url).then(data => {
+      setData(data);
+      setLoading(false);
+    })
+  }, [url, setData])
+
+  if(loading) return <h3>Loading...</h3>
   return (
-    <div>Subjects list here</div>
+    <div>
+      <div>Subjects list here</div>
+      <div className='flex justify-center mt-4'>
+          <Paginator pageChanged={setPageIndex} 
+            currentPage={params.pageIndex} pageCount={data.pageCount} />
+      </div>
+    </div>
   )
 }
