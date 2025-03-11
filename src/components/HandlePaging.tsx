1528c7dd0bc73853044bc26b22d501ddd2c2dbf9
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-03-11 17:05:52 +0700

    apply css for semester navbar

--- src/components/HandlePaging.tsx
+++ src/components/HandlePaging.tsx
@@ -4,37 +4,36 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 type Props = {
   visibleItems: number;
   totalItems: number;
+  initialItems: number;
   handleSeeMore: () => void;
   handleSeeLess: () => void;
 };
 export default function HandlePaging({
   visibleItems,
   totalItems,
+  initialItems,
   handleSeeMore,
   handleSeeLess,
 }: Props) {
   return (
-    <div>
-      {visibleItems < totalItems ? (
-        <div className="mt-8 flex justify-center">
-          <button
-            className="text-logo text-lg hover:underline flex flex-row items-center gap-2"
-            onClick={handleSeeMore}
-          >
-            <div>See More</div>
-            <FaChevronDown color="gray" />
-          </button>
-        </div>
-      ) : (
-        <div className="mt-8 flex justify-center">
-          <button
-            className="text-logo text-lg hover:underline flex flex-row items-center gap-2"
-            onClick={handleSeeLess}
-          >
-            <div>See Less</div>
-            <FaChevronUp color="gray" />
-          </button>
-        </div>
+    <div className="mt-8 flex justify-center">
+      {totalItems > initialItems && (
+        <button
+          className="text-logo text-lg hover:underline flex flex-row items-center gap-2"
+          onClick={visibleItems < totalItems ? handleSeeMore : handleSeeLess}
+        >
+          {visibleItems < totalItems ? (
+            <>
+              <div>See More</div>
+              <FaChevronDown color="gray" />
+            </>
+          ) : (
+            <>
+              <div>See Less</div>
+              <FaChevronUp color="gray" />
+            </>
+          )}
+        </button>
       )}
     </div>
   );
